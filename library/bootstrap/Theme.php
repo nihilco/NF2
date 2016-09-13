1b4527196be78f703d8aff1ec7df024600a1187b
Uriah M. Clemmer IV <uriah@nihil.co>
2016-09-13 14:13:24 -0400

    Added bootstrap loading of theme

---
+++ library/bootstrap/Theme.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace app\library\bootstrap;
+
+use Yii;
+use yii\base\BootstrapInterface;
+
+/*
+/* The base class that you use to retrieve the settings from the database
+*/
+
+class Theme implements BootstrapInterface {
+
+    private $db;
+
+    public function __construct() {
+        $this->db = Yii::$app->db;
+    }
+
+    /**
+     * Bootstrap method to be called during application bootstrap stage.
+     * Loads all the settings into the Yii::$app->params array
+     * @param Application $app the application currently running
+     */
+
+    public function bootstrap($app) {
+
+        // Get settings from database
+        $sql = $this->db->createCommand("SELECT slug FROM core_themes WHERE `active` = 1 LIMIT 1");
+        $result = $sql->queryOne();
+
+        if ($result) {
+            $themeSlug = $result['slug'];
+            Yii::$app->set('view',
+                [
+                    'class' => 'yii\web\View',
+                    'theme' => [
+                        'basePath' => '@app/themes/basic' . $themeSlug,
+                        'baseUrl' => '@web/themes/' . $themeSlug,
+                        'pathMap' => [
+                            '@app/modules' => '@app/themes/' . $themeSlug . '/modules',
+                        ],
+                    ]
+                ]
+            );
+        }
+
+    }
+
+}
