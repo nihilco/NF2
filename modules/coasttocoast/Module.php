67e97e89b1680783ac52aad2db07b3ee6e102109
Uriah M. Clemmer IV <uriah@nihil.co>
2016-09-14 12:51:07 -0400

    Created specific modules for themes

---
+++ modules/coasttocoast/Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\modules\coasttocoast;
+
+/**
+ * coasttocoast module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'app\modules\coasttocoast\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
---
+++ modules/tatetownship/controllers/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\modules\tatetownship\controllers;
+
+use yii\web\Controller;
+
+/**
+ * Default controller for the `tatetownship` module
+ */
+class DefaultController extends Controller
+{
+    public $layout = '@app/modules/core/views/layouts/page';
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
