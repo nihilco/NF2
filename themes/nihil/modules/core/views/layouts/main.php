35db14788fa2c0c940542de68618d495bffc9ed2
Uriah M. Clemmer IV <uriah@nihil.co>
2017-01-03 18:56:32 -0500

    Added Piwik tracking

--- themes/nihil/modules/core/views/layouts/main.php
+++ themes/nihil/modules/core/views/layouts/main.php
@@ -135,6 +135,21 @@ NihilAsset::register($this);
     </div>
 
 <?php $this->endBody() ?>
+<!-- Piwik -->
+<script type="text/javascript">
+var _paq = _paq || [];
+// tracker methods like "setCustomDimension" should be called before "trackPageView"
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+    var u="//pluto.nihil.co/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+})();
+</script>
+<!-- End Piwik Code -->
 </body>
 </html>
 <?php $this->endPage() ?>
